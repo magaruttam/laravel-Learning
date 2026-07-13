675333c80614a00f64ba6fb0762744155727f207
Uttam Pulami <pulamimagaruttam@gmail.com>
2026-07-13 19:20:33 +0545

    do some changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     function getUser(){
-        return "Uttam Pulami";
+        return DB::select("select * from users");
     }
 
     function getName($id,$name){
